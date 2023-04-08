d8b679ca40d74c8f7c51ddba6fa98421c36e001b
FFD <353130594@qq.com>
2023-04-08 22:05:35 +0800

    fixed bugs and able to run in did

--- test_dataloader.py
+++ test_dataloader.py
@@ -39,16 +39,20 @@ Rain_100L_data_path = '/data/ProjectData/Derain/Rain100L'
 Rain_DID_test_path = '/data/ProjectData/Derain/DID-Data/Test/'
 Rain_DID_train_path = '/data/ProjectData/Derain/DID-Data/Train/'
 
+Rain_Rain200H_test_path = '/data/ProjectData/Derain/Rain200H/test/'
+Rain_Rain200H_train_path = '/data/ProjectData/Derain/Rain200H/train/'
+
+Rain_SPA_test_path = '/data/ProjectData/Derain/DID-Data/Test/'
+Rain_SPA_train_path = '/data/ProjectData/Derain/DID-Data/Train/'
+
+
 #  I-HAZE
 I_HAZE_data_path = '/home/huangjiehui/Project/DerainNet/JackData/I-HAZE/hazy'
-
 #  O-HAZE
 O_HAZE_data_path = '/data1/hjh/ProjectData/Defogging/O-HAZE/hazy'
-
 #  SOTS-Ondoor
 SOTS_Ondoor_data_path = '/data1/hjh/ProjectData/Defogging/SOTS/Outdoor/hazy'
 SOTS_O_log_path = '/home/jack/Project/Derain/CTPNet/hjhDerain/Logs/Train_OTS2'
-
 #  SOTS-Indoor
 SOTS_Indoor_data_path = '/data1/hjh/ProjectData/Defogging/SOTS/Indoor/hazy'
 
@@ -76,17 +80,15 @@ def progress(y_origin):
 
 
 class Dataset_Rain200(udata.Dataset):
-    def __init__(self, data_path='.'):
+    def __init__(self, data_style='train'):
+        data_path = eval(f'Rain_Rain200H_{data_style}_path')
         super(Dataset_Rain200, self).__init__()
         self.data_path = data_path
-        if ('200' in self.data_path):
-            self.target_names =  data_path+'/norain/'
-            self.input_names =  data_path+'/rain/'
-        elif  ('100' in self.data_path):
-            self.target_names =  data_path + '/'
-            self.input_names =  data_path+'/rainy/'
+        self.target_names =  data_path+'/norain/'
+        self.input_names =  data_path+'/rain/'
+        self.ls1 = os.listdir(data_path + "rain")
     def __len__(self):
-        return len(self.input_names)
+        return len(self.ls1)
     def __getitem__(self, index):
         if ('200' in self.data_path):
             target_names = self.target_names + f'norain-{index+1}.png'
@@ -100,32 +102,24 @@ class Dataset_Rain200(udata.Dataset):
         gt = progress(gt)
         y = progress(y_origin)
         return y, gt
-#  ITS
-# ITS_data_path = '/home/huangjiehui/Project/DerainNet/JackData/ITS/train'
-# log_path = '/home/huangjiehui/Project/DerainNet/Logs'
 
 class Dataset_Rain200H(Dataset_Rain200):
     pass 
 
+
 class Dataset_Rain100(udata.Dataset):
-    def __init__(self, data_path='.'):
+    def __init__(self, data_style='train'):
+        data_path = eval(f'Rain_Rain200H_{data_style}_path')
         super(Dataset_Rain100, self).__init__()
         self.data_path = data_path
-        if ('200' in self.data_path):
-            self.target_names =  data_path+'/norain/'
-            self.input_names =  data_path+'/rain/'
-        elif  ('100' in self.data_path):
-            self.target_names =  data_path + '/norain/'
-            self.input_names =  data_path+'/rainy/'
+        self.target_names =  data_path+'/norain/'
+        self.input_names =  data_path+'/rain/'
+        self.ls1 = os.listdir(data_path + "rain")
     def __len__(self):
-        return len(self.input_names)
+        return len(self.ls1)
     def __getitem__(self, index):
-        if ('200' in self.data_path):
-            target_names = self.target_names + f'norain-{index+1}.png'
-            input_names =  self.input_names + f'norain-{index+1}x2.png'
-        elif  ('100' in self.data_path):
-            target_names = self.target_names + 'norain-%03d.png'%(index+1)
-            input_names = self.input_names + 'rain-%03d.png'%(index+1)
+        target_names = self.target_names + 'norain-%03d.png'%(index+1)
+        input_names = self.input_names + 'rain-%03d.png'%(index+1)
             
         y_origin = cv2.imread(os.path.join(input_names))
         gt = cv2.imread(os.path.join(target_names))
@@ -133,68 +127,29 @@ class Dataset_Rain100(udata.Dataset):
         y = progress(y_origin)
         return y, gt
 
-class Dataset(udata.Dataset):
-    def __init__(self, data_path=SOTS_Ondoor_data_path):
-        super(Dataset, self).__init__()
-        self.data_path = data_path
-        self.img_names =  os.listdir(data_path)
-        self.num = 6    # 把一张一张大图分为num份
-    def __len__(self):
-        if ('O-HAZE' in self.data_path or 'I-HAZE'in self.data_path) :
-            return len(self.img_names)*self.num
-        return len(self.img_names)
-    def __getitem__(self, index):
-        # target
-        # GT path with target data
-        if 'O-HAZE' in self.data_path or 'I-HAZE'in self.data_path :
-            # !!!!!!RGB
-            y_origin = cv2.imread(os.path.join(self.data_path, self.img_names[index//self.num]))
-            gt_path = os.path.join(self.data_path, self.img_names[index//self.num]).replace("hazy",'GT')   # #  I-HAZE O-HAZE
-            gt = cv2.imread(gt_path)
-            gt= patchify(gt.transpose(2, 0, 1),(3,96,96),step=96).reshape(-1,3,96,96)/255
-            y= patchify(y_origin.transpose(2, 0, 1),(3,96,96),step=96).reshape(-1,3,96,96)/255
-            # gt,y = progress(gt),progress(y)
-            l = gt.shape[0]//(self.num-1)
-            return y[index%self.num*l : (index%self.num+1)*l ,:,:,:], gt[index%self.num*l : (index%self.num+1)*l ,:,:,:]
-        
-        elif ('SOTS/O' in self.data_path or 'SOTS/I'in self.data_path) :
-            y_origin = cv2.imread(os.path.join(self.data_path, self.img_names[index]))
-            gt_path = os.path.join(self.data_path.replace('hazy','nohazy'), self.img_names[index].split('_')[0]+'.png') #  SOTS-Ondoor SOTS-Indoor 
-            # gt = progress(cv2.imread(gt_path))
-            y = progress(y_origin)
-            if 'SOTS/Indoor' in self.data_path:
-                gt = progress(cv2.imread(gt_path))[:,:,10:458,10:618]  # #  SOTS-Indoor
-            else:
-                gt = progress(cv2.imread(gt_path))
-            return y, gt
 
-class Dataset_DID_800(udata.Dataset):
-    def __init__(self, data_path='.'):
-        super(Dataset_DID_800, self).__init__()
-        self.data_path = data_path
-        ls1 = os.listdir(data_path)
-        ls1 = [data_path+'/'+i for i in ls1]
-        self.keys = ls1
-    def __len__(self):
-        if 'DID' in self.data_path:
-            return 1000
-        else:
-            return len(self.keys)
-    def __getitem__(self, index):
-        key = self.keys[index]
-        img = cv2.imread(key)
-        b, g, r = cv2.split(img)
-        img = cv2.merge([r, g, b])
-        img = np.float32(normalize(img)).transpose(2,0,1)
-        target = img[:,:,0:img.shape[2]//2]
-        input = img[:,:,img.shape[2]//2:]
-        target,input =np.expand_dims(target, 0), np.expand_dims(input, 0)
 
-        high = target.shape[2]//32
-        wight = target.shape[3]//32
-        target = target[:,:, 0:high*32, 0:wight*32]
-        
-        high = input.shape[2]//32
-        wight = input.shape[3]//32
-        input = input[:,:, 0:high*32, 0:wight*32]
-        return torch.Tensor(input), torch.Tensor(target)
+
+class Dataset_DID(udata.Dataset):
+    def __init__(self, data_style):
+        super(Dataset_DID, self).__init__()
+        data_path = eval(f'Rain_DID_{data_style}_path')
+        target_names =  data_path + 'norain/' 
+        input_names =  data_path+'rain/'
+        ls1 = os.listdir(data_path + "rain")
+        # ls1 = [target_names+'/'+i for i in ls1]
+        self.targets =  [target_names + i for i in ls1]
+        self.inputs =  [input_names + i for i in ls1]
+    def __len__(self):
+            return len(self.targets)
+    def __getitem__(self, index):
+        target_names = self.targets[index]
+        input_names = self.inputs[index]
+        y_origin = cv2.imread(input_names)
+        # print(input_names,target_names)
+        gt = cv2.imread(os.path.join(target_names))
+        gt = progress(gt).squeeze()
+        y = progress(y_origin).squeeze()
+        # print(y.shape, gt.shape)
+
+        return y, gt
